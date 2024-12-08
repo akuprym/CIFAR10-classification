612077791225087bc1fe2c6510f083a87badbf48
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-12-08 23:09:28 +0300

    Initial commit

---
+++ cifar10-classification.py
@@ -0,0 +1,24 @@
+import os
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter)
+from dataclasses import dataclass
+
+SEED_VALUE = 42
+
+random.seed(SEED_VALUE)
+np.random.seed(SEED_VALUE)
+tf.random.set_seed(SEED_VALUE)
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+print(X_train.shape)
+print(X_test.shape)
